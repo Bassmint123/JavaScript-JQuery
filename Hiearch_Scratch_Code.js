3e9c502c87189c5b0c19fdae13bce30a936e3c9b
Bassmint123 <bradb@cimstrat.com>
2014-10-10 18:41:33 -0500

    Hierarchial Structuring in JSON
    Basis structures in JSON

---
+++ Hiearch_Scratch_Code.js
@@ -0,0 +1,34 @@
+var myObject = {
+   “first”: “Brad”,
+   “last”: “Beiermann”,
+   “age”: 39,
+   “interests”: [ "Keyhole", "Jazz", "Hacking" ],
+   “favorites”: {
+      “color”: “Blue”,
+      “sport”: “Squirt Gunning”,
+      “food”: “Tree Bark”
+    },
+ “skills”: [
+  {
+   “category”: “JavaScript”,
+   “tests”: [
+       { "name": "One", "score": 90 },
+       { "name": "Two", "score": 96 }
+    ]
+  },
+  {
+   “category”: “CouchDB”,
+   “tests”: [
+   { "name": "One", "score": 79 },
+   { "name": "Two", "score": 84 }
+   ]
+  },
+  {
+   “category”: “Node.js”,
+   “tests”: [
+       { "name": "One", "score": 97 },
+       { "name": "Two", "score": 93 }
+    ]
+   }
+ ]
+};
